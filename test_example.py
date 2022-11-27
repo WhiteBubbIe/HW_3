bd1ed2c941bafa0a80b06d1489dc875ad6ce3f32
WhiteBubbIe <0kiserr@gmail.com>
2022-11-27 15:35:12 +0500

    complex refactoring

--- test_example.py
+++ test_example.py
@@ -1,4 +1,11 @@
 
-def test_example():
-    assert 3 > 1
+def test_app():
+    assert 10 != 15
 
+# новый тест
+def new_test_app_01():
+    assert 2 != 5
+
+# еще новый тест
+def new_test_app_02():
+    assert 0 < 5
